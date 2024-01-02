6903570b3596559abca26bb429b35d39a2e3c3b1
Barak <18008770+Lezyes@users.noreply.github.com>
2024-01-02 04:06:04 +0200

    Update web.cljs

--- src/pushfight/web.cljs
+++ src/pushfight/web.cljs
@@ -2,15 +2,39 @@
   (:require 
     [reagent.core :as r :refer [with-let]]
     [reagent.dom :as rdom]
-    [rewig.components :refer [box row column]]))
+    [rewig.components :refer [box row column]]
+    [pushfight.core :as pf]
+    [clojure.string :as string]))
 
+
+
+
+(defn cell->box [cell]
+  (let [piece (:piece cell)
+        anchored? (:anchored? cell)
+        team-color 0
+        piece     (cond 
+                    (nil? piece)    ""
+                    (pf/pusher? piece) ""
+                    :else              "")]))
+                    
+
+(defn display-board [board]
+  [column
+    (for [rn (range (count board)) 
+          :let [r (get board rn)]]
+      [row [
+            (for [cn (range (count r))
+                  :let [cell (get r cn)
+                        cell-key (string/join "-" ["cell" rn cn])]]
+              ^{:key cell-key}[box {:margin 2 :click! #() :css {:background-color (or (get-in cell [:piece :team]) "coral")}} (pf/cell->emoji cell)])]])])
 
 (defn app []
-  (with-let [x (r/atom 0)]
-    [column
-     [
-      [row {:click! #(swap! x inc)} (str "clicked here " @x " times")]
-      [row "asdasd"]]]))
+  (with-let [board (r/atom pf/sample-board)]
+    (let [k (atom 0)]
+      [box
+        [[box {:css {:color "red"  }} ""]
+         (display-board @board)]])))
 
 
 (defn ^:export main []
